0ed70a977a318805a9bc7f9c0f27bdd5dff352bc
onzl-c <dmswisl1017@gmail.com>
2024-09-30 21:28:31 +0900

    done

--- src/main/java/com/shanep/controller/UrlController.java
+++ src/main/java/com/shanep/controller/UrlController.java
@@ -1,13 +1,78 @@
 package com.shanep.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shanep.model.Board;
+import com.shanep.service.BoardService;
 
 @Controller
 public class UrlController {
-
-    @GetMapping("/")
-    public String home() {
-        return "index"; // index.html 템플릿 반환
-    }
+	
+	@Autowired
+	BoardService boardService;
+	
+	@GetMapping(value={"/","board"})
+	public ModelAndView boardList(ModelAndView mav) {
+		mav.setViewName("page/board");
+		return mav;
+	}
+	
+	@GetMapping("board/{boardno}")
+	public ModelAndView board(
+			ModelAndView mav
+			, @PathVariable int boardno) {
+		mav.setViewName("page/view");
+		Board board = (Board) boardService.retrieveBoard(boardno).getPayload();
+		mav.addObject(board);
+		return mav;
+	}
+	@GetMapping("board/{boardno}/delete")
+	public ModelAndView delete(
+			ModelAndView mav
+			, @PathVariable int boardno) {
+		mav.setViewName("redirect:/");
+		boardService.deleteBoard(boardno);
+		return mav;
+	}
+	
+	@GetMapping("board/write")
+	public ModelAndView write(ModelAndView mav) {
+		mav.setViewName("page/write");
+		return mav;
+	}
+	@PostMapping("board/write")
+	public ModelAndView writeView(
+			ModelAndView mav
+			,@ModelAttribute Board board) {
+		mav.setViewName("redirect:/");
+		boardService.createBoard(board);
+		return mav;
+	}
+	
+	@GetMapping("board/{boardno}/edit")
+	public ModelAndView editView(
+			ModelAndView mav
+			, @PathVariable int boardno) {
+		mav.setViewName("page/edit");
+		Board board = (Board) boardService.retrieveBoard(boardno).getPayload();
+		mav.addObject(board);
+		return mav;
+	}
+	@PostMapping("board/{boardno}/edit")
+	public ModelAndView edit(
+			ModelAndView mav
+			, @PathVariable int boardno
+			, @ModelAttribute Board board) {
+		mav.setViewName(String.format("redirect:/board/%d", boardno));
+		boardService.updateBoard(board);
+		return mav;
+	}
 }
--- src/main/java/com/shanep/enumpkg/ServiceResult.java
+++ src/main/java/com/shanep/enumpkg/ServiceResult.java
@@ -1,26 +1,5 @@
 package com.shanep.enumpkg;
 
-public class ServiceResult {
-    public static final String NOTEXIST = "NOT_EXIST";
-    
-    private boolean success;
-    private String message;
-
-    // Getters and Setters
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+public enum ServiceResult {
+	OK, FAIL, NOTEXIST, HASCHILD
 }
